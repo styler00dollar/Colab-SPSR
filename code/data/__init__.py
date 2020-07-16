2978151c018684d38527e96bc7fa7236abf70103
BlueAmulet <43395286+BlueAmulet@users.noreply.github.com>
2020-07-16 13:37:40 -0600

    Add LRHROTF dataset

--- code/data/__init__.py
+++ code/data/__init__.py
@@ -26,6 +26,8 @@ def create_dataset(dataset_opt):
         from data.LR_dataset import LRDataset as D
     elif mode == 'LRHR':
         from data.LRHR_dataset import LRHRDataset as D
+    elif mode == 'LRHROTF':
+        from data.LRHROTF_dataset import LRHRDataset as D
     else:
         raise NotImplementedError('Dataset [{:s}] is not recognized.'.format(mode))
     dataset = D(dataset_opt)
